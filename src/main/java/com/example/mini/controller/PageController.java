40dd3dfd3b1d3ce316eccd9f4183154ad5b72bea
mark hwang <ai.markhwang@gmail.com>
2022-01-16 15:18:54 +0900

    영화 정보

--- src/main/java/com/example/mini/controller/PageController.java
+++ src/main/java/com/example/mini/controller/PageController.java
@@ -1,14 +1,16 @@
 package com.example.mini.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
 
-    @GetMapping("/main")
+    @GetMapping("/")
     public String main(){
-        return "aaaa/main";
+        return "main";
     }
 
     @GetMapping("/movie")
